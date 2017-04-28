ea029d771c78f03ae060fcdc6edb2bdb246fd8b0
Stephanie Lam <stephaniewylam@hotmail.com>
2017-04-28 16:36:01 +0200

    icon added + link href

--- Form/js/main.js
+++ Form/js/main.js
@@ -9,8 +9,7 @@ function myFunction() {
 (function($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    // Smooth scrolling using jQuery easing    $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
